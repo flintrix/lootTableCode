05da0f8fa60ed9d3df1a810d8c457a4f2e184e1d
flintrix <danielhartog258@gmail.com>
2025-10-17 21:45:16 +0200

    init commit

--- src/me/flintrix/Main.java
+++ src/me/flintrix/Main.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        String[] lootTableCommon = {"gold", "gems", "a sword", "an axe", "a shield", "a bow", "arrows", "a magic wand", "refined gold"};
+        String[] lootTableCommon = {"gold ingot", "gem", "sword", "axe", "shield", "bow", "arrow", "magic wand", "refined gold ingot"};
         double[] commonChances = {0.8, 0.4, 0.6, 0.6, 0.3, 0.6, 0.9, 0.2, 0.1};
         lootTable(lootTableCommon, commonChances);
     }
@@ -14,7 +14,16 @@ public class Main {
         for (int i = 0; i < lootTable.length; i++){
             double roll = random.nextDouble();
             if (roll < chances[i]){
-                System.out.printf("you aquired %s%n", lootTable[i]);
+                double roll1 = random.nextDouble();
+                String rarity = "common";
+                if (roll1 < 0.1){
+                    rarity = "legendary";
+                } else if (roll1 < 0.3) {
+                    rarity = "epic";
+                } else if (roll1 < 0.6) {
+                    rarity = "rare";
+                }
+                System.out.printf("you aquired a(n) %s %s%n", rarity, lootTable[i]);
             }
         }
     }

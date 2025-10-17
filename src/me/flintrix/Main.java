58b42733bd296d9718c11a94ab54ed73897220a2
flintrix <danielhartog258@gmail.com>
2025-10-17 21:49:27 +0200

    init commit

--- src/me/flintrix/Main.java
+++ src/me/flintrix/Main.java
@@ -8,22 +8,27 @@ public class Main {
         double[] commonChances = {0.8, 0.4, 0.6, 0.6, 0.3, 0.6, 0.9, 0.2, 0.1};
         lootTable(lootTableCommon, commonChances);
     }
-    public static void lootTable(String[] lootTable, double[] chances){
+
+    public static void lootTable(String[] lootTable, double[] chances) {
         Random random = new Random();
 
-        for (int i = 0; i < lootTable.length; i++){
+        for (int i = 0; i < lootTable.length; i++) {
             double roll = random.nextDouble();
-            if (roll < chances[i]){
+            if (roll < chances[i]) {
                 double roll1 = random.nextDouble();
+                String color = "\u001B[0m";
                 String rarity = "common";
-                if (roll1 < 0.1){
+                if (roll1 < 0.1) {
                     rarity = "legendary";
+                    color = "\u001B[33m";
                 } else if (roll1 < 0.3) {
                     rarity = "epic";
+                    color = "\u001B[35m";
                 } else if (roll1 < 0.6) {
                     rarity = "rare";
+                    color = "\u001B[34m";
                 }
-                System.out.printf("you aquired a(n) %s %s%n", rarity, lootTable[i]);
+                System.out.printf(color + "You acquired a(n) %s %s%n", rarity, lootTable[i]);
             }
         }
     }
